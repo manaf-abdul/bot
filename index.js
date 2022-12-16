6cb6f699f63044311f7ae850c00b1fce3135c7c3
manaf-abdul <abdulmanafp1996@gmail.com>
2022-12-16 19:05:23 +0530

    CHANGES

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 
 let messageContent = [
-  "Hello,Thank you for your message. Owner is currently unavailable, I am Athena virtual assistant of Manaf and am here to help with any questions or concerns you may have. Please let me know how I can assist you and I will do my best to help.",
+  "Hello,Thank you for your message. Owner is currently unavailable, I am Athena(AI using CHATGPT) virtual assistant and am here to help with any questions or concerns you may have. Please let me know how I can assist you and I will do my best to help.",
   "Thank you for reaching out! Owner is currently unavailable, but I am happy to assist you in any way that I can. How may I help you today?",
   "Hello and thank you for reaching out! Owner is currently unavailable, but I am here to assist you in any way that I can. Please let me know if you have any questions or need assistance with anything. I will do my best to help or pass along your message to the owner as soon as possible. Thank you for your understanding.",
-  "Hello and welcome! I am the Owner's virtual assistant - Athena, and I am here to help with any questions or needs you may have. The owner is currently unavailable, but I will do my best to assist you in the meantime. Please let me know if there is anything I can help with. Thank you for your message.",
-  "Hello! Thank you for reaching out. It's great to meet you. I'm Athena, virtual assistant of Owner, and I'm here to help with any questions or concerns you may have. Is there anything specific you'd like to chat about today? I'm all ears!",
-  "Hello and welcome! Thank you for your message. I am Athena, virtual assistant of Owner here to help with any questions or concerns you may have. Please let me know how I can assist you. I look forward to chatting with you.",
+  "Hello and welcome! I am the Owner's virtual assistant - Athena(AI using CHATGPT), and I am here to help with any questions or needs you may have. The owner is currently unavailable, but I will do my best to assist you in the meantime. Please let me know if there is anything I can help with. Thank you for your message.",
+  "Hello! Thank you for reaching out. It's great to meet you. I'm Athena(AI using CHATGPT), virtual assistant of Owner, and I'm here to help with any questions or concerns you may have. Is there anything specific you'd like to chat about today? I'm all ears!",
+  "Hello and welcome! Thank you for your message. I am Athena(AI using CHATGPT), virtual assistant of Owner here to help with any questions or concerns you may have. Please let me know how I can assist you. I look forward to chatting with you.",
   "CHATGPT is Active",
   "Bot is on TEST...",
   "Jarvis Activated",
